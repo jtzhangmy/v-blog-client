661e46f84e4d4ce20f83877941f125d3b3236ad8
jtzhangmy <aspirine839716326@gmail.com>
2017-01-07 23:01:51 +0800

    change style

--- src/component/articleDetail.vue
+++ src/component/articleDetail.vue
@@ -26,10 +26,12 @@
         margin: 10px 0 15px 0;
         border-collapse: collapse;
     }
+
     td,th {
         border: 1px solid #ddd;
         padding: 3px 10px;
     }
+
     th {
         padding: 5px 10px;
     }
@@ -37,49 +39,62 @@
     a {
         color: #0069d6;
     }
+
     a:hover {
         color: #0050a3;
         text-decoration: none;
     }
+
     a img {
         border: none;
     }
+
     p {
         margin-bottom: 9px;
     }
+    
     h1,h2,h3,h4,h5,h6 {
         line-height: 36px;
     }
+    
     h1 {
         margin-bottom: 18px;
         font-size: 30px;
     }
+    
     h2 {
         font-size: 24px;
     }
+    
     h3 {
         font-size: 18px;
     }
+    
     h4 {
         font-size: 16px;
     }
+    
     h5 {
         font-size: 14px;
     }
+    
     h6 {
         font-size: 13px;
     }
+    
     hr {
         margin: 0 0 19px;
         border: 0;
         border-bottom: 1px solid #ccc;
     }
+    
     blockquote {
         padding: 13px 13px 21px 15px;
         margin-bottom: 18px;
         font-family:georgia,serif;
         font-style: italic;
     }
+    
     blockquote:before {
         content:"\201C";
         font-size:40px;
@@ -87,6 +102,7 @@
         font-family:georgia,serif;
         color:#eee;
     }
+    
     blockquote p {
         font-size: 14px;
         font-weight: 300;
@@ -94,9 +110,11 @@
         margin-bottom: 0;
         font-style: italic;
     }
+    
     code, pre {
         font-family: Monaco, Andale Mono, Courier New, monospace;
     }
+    
     code {
         background-color: #fee9cc;
         color: rgba(0, 0, 0, 0.75);
@@ -105,7 +123,9 @@
         -webkit-border-radius: 3px;
         -moz-border-radius: 3px;
         border-radius: 3px;
+    
     }
+
     pre {
         display: block;
         padding: 14px;
@@ -116,29 +136,35 @@
         white-space: pre-wrap;
         word-wrap: break-word;
     }
+    
     pre code {
         background-color: #fff;
         color:#737373;
         font-size: 11px;
         padding: 0;
     }
+    
     sup {
         font-size: 0.83em;
         vertical-align: super;
         line-height: 0;
     }
+    
     img {
         display: block;
     }
+    
     * {
         -webkit-print-color-adjust: exact;
     }
+    
     @media screen and (min-width: 914px) {
         body {
             width: 854px;
             margin:10px auto;
         }
     }
+    
     @media print {
         body,code,pre code,h1,h2,h3,h4,h5,h6 {
             color: black;
